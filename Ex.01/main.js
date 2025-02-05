f19a76fb15ec02c6c1b84fca88d4e6c833e290ce
maurograndaoramos <mauro.dgramos@gmail.com>
2025-02-05 21:51:53 +0000

    Ex.01 - Add initial user, transactions, and products data structures

---
+++ Ex.01/main.js
@@ -0,0 +1,32 @@
+const user = {
+    name : "",
+    transactions : [],
+    points : 0,
+    email : "",
+    password : "",
+    address : {
+        street : "",
+        number : 0,
+        city : "",
+        country : "",
+        zip : 0
+    }
+};
+
+const transactions = {
+    uID: 1,
+    products: [],
+    totalPrice: 0
+};
+
+const products = {
+    uID: 1,
+    name: "",
+    listPrice: 0,
+    actualPrice: 0,
+    quantity: 0
+};
+
+
+
+
