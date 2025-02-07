94ea1487be301b96b0b34554b83d59abce89b1c2
maurograndaoramos <mauro.dgramos@gmail.com>
2025-02-07 19:56:33 +0000

    Add Car class extending Vehicle and instantiate myCar

--- Aula03/main.ts
+++ Aula03/main.ts
@@ -1,3 +1,13 @@
 class Vehicle {
     constructor(public brand: string, public model: string, public year: string) { }
 }
+
+
+class Car extends Vehicle {
+    constructor(brand: string, model: string, year: string, public doors: number) {
+        super(brand, model, year);
+    }
+}
+
+const myCar = new Car('Honda', 'Civic', '2007', 4);
+console.log(myCar);
